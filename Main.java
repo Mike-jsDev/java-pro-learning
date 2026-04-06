8c79ccdc59c6465e7fa3f358d90ee93c8071cdfa
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-04-06 18:31:19 +0200

    Initial commit: add README.md, .gitignore and Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java! Это develop branch.");
+        System.out.println("Начало обучения Java Pro");
+    }
+}
