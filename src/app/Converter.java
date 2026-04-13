6ae070a0d15a2b91832449577a14bef97249c308
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-04-13 19:04:51 +0200

    Add Celsius-Fahrenheit conversion feature

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -17,4 +17,8 @@ public class Converter {
     public static double lbToKg(double lb) {
         return lb / 2.20462;
     }
+
+    public static double celsiusToFahrenheit(double c) {return (c * 9/5) + 32;}
+
+    public static double fahrenheitToCelsius(double f) {return (f - 32) * 5/9;}
 }
